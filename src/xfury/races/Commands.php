dd8d77faa9e125be54be6601bc927dbaa8ff5328
Jonathanftw <romerojonathan280@gmail.com>
2016-04-26 16:50:35 -0700

    Update Commands.php

--- src/xfury/races/Commands.php
+++ src/xfury/races/Commands.php
@@ -22,27 +22,27 @@ class Commands{
 		switch($fcmd){
 			case "race":
 				if(!$sender instanceof Player){
-					$sender->sendMessage(TextFormat::BLUE."[Race] ".TextFormat::RED."Please run this command ingame!");
+					$sender->sendMessage(TextFormat::GOLD."[Race] ".TextFormat::YELLOW."Please run this command ingame!");
 					return;
 				}
 				if($this->plugin->hasRace($sender->getName()) == true){
-					$sender->sendMessage(TextFormat::BLUE."[Race] ".TextFormat::RED."Your race cannot be changed!");
+					$sender->sendMessage(TextFormat::GOLD."[Race] ".TextFormat::YELLOW."Your race cannot be changed!");
 					return;
 				}
 				if(count($args) != 1){
-					$sender->sendMessage(TextFormat::RED."Usage: /race <runner:jumper:miner>");
+					$sender->sendMessage(TextFormat::YELLOW."Usage: /race <runner:jumper:miner>");
 					return;
 				}
 				switch(strtolower($args[0])){
 					case "runner":
 						if(!isset($this->plugin->race[$sender->getName()])){
-							$sender->sendMessage(TextFormat::BLUE."[Races] ".TextFormat::RED."Are you sure you want to be a runner? Once you select your race, you cannot switch it! Type /race runner again to choose!");
+							$sender->sendMessage(TextFormat::GOLD."[Races] ".TextFormat::YELLOW."Are you sure you want to be a runner? Once you select your race, you cannot switch it! Type /race runner again to choose!");
 							$this->plugin->race[$sender->getName()]["race"] = "runner";
 							$this->plugin->race[$sender->getName()]["timestamp"] = time();
 							return;
 						}
 						if($this->plugin->race[$sender->getName()]["race"] != "runner"){
-							$sender->sendMessage(TextFormat::BLUE."[Races] ".TextFormat::RED."Are you sure you want to be a runner? Once you select your race, you cannot switch it! Type /race runner again to choose!");
+							$sender->sendMessage(TextFormat::GOLD."[Races] ".TextFormat::YELLOW."Are you sure you want to be a runner? Once you select your race, you cannot switch it! Type /race runner again to choose!");
 							$this->plugin->race[$sender->getName()]["race"] = "runner";
 							$this->plugin->race[$sender->getName()]["timestamp"] = time();
 							return;
@@ -76,21 +76,21 @@ class Commands{
 							return;
 						}
 						if($this->plugin->race[$sender->getName()]["race"] != "miner"){
-							$sender->sendMessage(TextFormat::BLUE."[Races] ".TextFormat::RED."Are you sure you want to be a miner? Once you select your race, you cannot switch it! Type /race miner again to choose!");
+							$sender->sendMessage(TextFormat::GOLD."[Races] ".TextFormat::YELLOW."Are you sure you want to be a miner? Once you select your race, you cannot switch it! Type /race miner again to choose!");
 							$this->plugin->race[$sender->getName()]["race"] = "miner";
 							$this->plugin->race[$sender->getName()]["timestamp"] = time();
 							return;
 						}
 						$this->plugin->setRace($sender->getName(), 2);
-						$sender->sendMessage(TextFormat::BLUE."[Race] ".TextFormat::GREEN."You are now a miner!");
+						$sender->sendMessage(TextFormat::GOLD."[Race] ".TextFormat::YELLOW."You are now a miner!");
 						unset($this->plugin->race[$sender->getName()]);
 					break;
 					default:
-						$sender->sendMessage(TextFormat::BLUE."Availible races:");
-						$sender->sendMessage(TextFormat::GREEN."Runner");
-						$sender->sendMessage(TextFormat::GREEN."Jumper");
-						$sender->sendMessage(TextFormat::GREEN."Miner");
-						$sender->sendMessage(TextFormat::RED."WARNING: Once your race is chosen, you cannot choose it again!");
+						$sender->sendMessage(TextFormat::GOLD."Availible races:");
+						$sender->sendMessage(TextFormat::YELLOW."Runner");
+						$sender->sendMessage(TextFormat::YELLOW."Jumper");
+						$sender->sendMessage(TextFormat::YELLOW."Miner");
+						$sender->sendMessage(TextFormat::YELLOW."WARNING: Once your race is chosen, you cannot choose it again!");
 					break;
 				}
 			break;
