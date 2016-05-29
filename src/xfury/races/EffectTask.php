ba929d55aaea98e1384fe5981596e7caeb0d7c77
Joe <JoeycraftPE@gmail.com>
2016-05-29 18:56:22 -0500

    Update EffectTask.php

--- src/xfury/races/EffectTask.php
+++ src/xfury/races/EffectTask.php
@@ -17,15 +17,12 @@ class EffectTask extends PluginTask{
 		foreach($this->plugin->getServer()->getOnlinePlayers() as $p){
 			if($this->plugin->hasRace($p->getName()) == true){
 				if($this->plugin->getRace($p->getName()) == 0){
-					$p->addEffect(Effect::getEffect(Effect::FIRE_RESISTANCE)->setDuration(20)->setAmplifier(1)->setVisible(false));
+					$p->addEffect(Effect::getEffect(Effect::JUMP_BOOST)->setDuration(20)->setAmplifier(2)->setVisible(false));
 				}
 				if($this->plugin->getRace($p->getName()) == 1){
-					$p->addEffect(Effect::getEffect(Effect::HEALTH_BOOST)->setDuration(20)->setAmplifier(1)->setVisible(false));
+					$p->addEffect(Effect::getEffect(Effect::HEALTH_BOOST)->setDuration(20)->setAmplifier(2)->setVisible(false));
 				}
 				if($this->plugin->getRace($p->getName()) == 2){
-					$p->addEffect(Effect::getEffect(Effect::REGENERATION)->setDuration(20)->setAmplifier(0)->setVisible(false));
-				}
-	                        if($this->plugin->getRace($p->getName()) == 3){
 					$p->addEffect(Effect::getEffect(Effect::SPEED)->setDuration(20)->setAmplifier(2)->setVisible(false));
 			}
 		}
