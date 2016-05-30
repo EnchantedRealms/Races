1275b7890ffe88b49dee1b94635d2a87c4422c35
Joe <JoeycraftPE@gmail.com>
2016-05-30 10:03:50 -0500

    Update EffectTask.php

--- src/xfury/races/EffectTask.php
+++ src/xfury/races/EffectTask.php
@@ -20,7 +20,7 @@ class EffectTask extends PluginTask{
 					$p->addEffect(Effect::getEffect(Effect::JUMP)->setDuration(20)->setAmplifier(1)->setVisible(false));
 				}
 				if($this->plugin->getRace($p->getName()) == 1){
-					$p->addEffect(Effect::getEffect(Effect::FIRE_RESISTANCE)->setDuration(20)->setAmplifier(1)->setVisible(false));
+					$p->addEffect(Effect::getEffect(Effect::HASTE)->setDuration(20)->setAmplifier(1)->setVisible(false));
 				}
 				if($this->plugin->getRace($p->getName()) == 2){
 					$p->addEffect(Effect::getEffect(Effect::SPEED)->setDuration(20)->setAmplifier(1)->setVisible(false));
